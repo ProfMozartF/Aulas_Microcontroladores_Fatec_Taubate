09e9d3bbc19b69d697b5dd94ee418561f23666af
MozartFM <154269492+MozartFM@users.noreply.github.com>
2025-06-17 20:29:55 -0300

    Mozart - Cleanup Dev

--- Exemplos_Logica_C/Enum.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-enum DiasSemana {DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO};
-
-int main() {
-    enum DiasSemana hoje = QUARTA;
-    printf("Hoje é %d\n", hoje);
-    return 0;
-}
